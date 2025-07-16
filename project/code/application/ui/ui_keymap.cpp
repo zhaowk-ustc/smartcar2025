0e914cfc049d52a14c27c8a21f5762e56c9e3acd
cole-andrew <1533115070@qq.com>
2025-07-16 17:41:41 +0800

    key

--- project/code/application/ui/ui_keymap.cpp
+++ project/code/application/ui/ui_keymap.cpp
@@ -3,8 +3,38 @@
 UI::Key UI::get_key_input()
 {
     // 实现具体的按键读取逻辑
+    uint16 voltage;
+    voltage = adc_convert(ADC0_CH00_P06_0);
 
-    return Key::NONE;
+    if (0 <= voltage and voltage <= 18)
+    { // voltage == 0
+        return Key::LEFT;
+    }
+    else if (19 <= voltage and voltage <= 60)
+    { // voltage == 36
+        return Key::UP;
+    }
+    else if (61 <= voltage and voltage <= 105)
+    { // voltage == 83
+        return Key::DOWN;
+    }
+    else if (106 <= voltage and voltage <= 156)
+    { // voltage == 127
+        return Key::RIGHT;
+    }
+    else if (157 <= voltage and voltage <= 220)
+    { // voltage == 185
+        return Key::ENTER;
+    }
+    // else if(221 <= voltage and voltage <= 255){ //voltage == 255
+    //     return Key::NONE;
+    // }
+    else
+    { // voltage == 255
+        return Key::NONE;
+    }
+
+    // return Key::NONE;
 }
 
 void UI::update_key_state()
@@ -22,23 +52,23 @@ void UI::handle_current_key()
     // 根据 current_key_ 调用相应的处理函数
     switch (current_key_)
     {
-        case Key::UP:
-            on_key_up();
-            break;
-        case Key::DOWN:
-            on_key_down();
-            break;
-        case Key::LEFT:
-            on_key_left();
-            break;
-        case Key::RIGHT:
-            on_key_right();
-            break;
-        case Key::ENTER:
-            on_key_enter();
-            break;
-        default:
-            break;
+    case Key::UP:
+        on_key_up();
+        break;
+    case Key::DOWN:
+        on_key_down();
+        break;
+    case Key::LEFT:
+        on_key_left();
+        break;
+    case Key::RIGHT:
+        on_key_right();
+        break;
+    case Key::ENTER:
+        on_key_enter();
+        break;
+    default:
+        break;
     }
 
     current_key_ = Key::NONE; // 处理完后重置按键状态
